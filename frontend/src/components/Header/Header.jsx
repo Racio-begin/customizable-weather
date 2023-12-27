c429b5305005cea3eb4fca166c1d436bbfad78f2
Nikita Averyanov <rac1onal@yandex.ru>
2023-12-27 22:37:48 +0300

    feat: добавить логотип сайта в хедер

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,8 +1,17 @@
+import logo from '../../images/logo.png';
+
 import './Header.css';
 
 function Header() {
 	return (
 		<header className="header">
+			<img
+				className="logo-icon"
+				width={220}
+				height={220}
+				src={logo}
+				alt="Логотип сайта"
+			/>
 			HEADER
 		</header>
 	);
